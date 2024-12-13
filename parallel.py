8ce18c2460f70d4d4e01e54f0b87f65a1937b561
Youssef Ahmed <yousseffcai@gmail.com>
2024-12-13 17:48:01 +0200

    finished save as page

--- parallel.py
+++ parallel.py
@@ -1,9 +1,14 @@
-import requests # type: ignore
-from tqdm import tqdm # type: ignore
+import requests
+from tqdm import tqdm
+import tkinter as tk
+from tkinter import filedialog
+from urllib.parse import urlparse
+
 
 def download_file(url, output_path):
     response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))  # Total size in bytes
+    total_size = int(response.headers.get(
+        'content-length', 0))
 
     with open(output_path, 'wb') as file, tqdm(
         desc="Downloading",
@@ -16,8 +21,31 @@ def download_file(url, output_path):
             file.write(data)
             progress_bar.update(len(data))
 
-# Example usage
+
+def handle_file_path(url):
+    path = requests.get(url, stream=True).headers.get(
+        "Content-Disposition") or "filename='UnKnown.xxx'"
+    path = path.split("filename=")[-1][1:-1]
+    extension = "." + path.split('.')[-1]
+    path = path.encode('latin1').decode('utf-8')
+    
+    root = tk.Tk()
+    root.withdraw()
+    root.lift()
+    root.attributes('-topmost', 1)
+
+    root.update()
+    file_path = filedialog.asksaveasfilename(
+        filetypes=[("All files", "*.*")],
+        defaultextension=extension,
+        initialfile=path)
+    return file_path
+
+
 if __name__ == "__main__":
-    download_url = input("Enter the direct download URL: ")
-    output_file_path = input("Enter the output file path: ")
-    download_file(download_url, output_file_path)
+    download_url = input("Enter the URL: ")
+    file_path = handle_file_path(download_url)
+    if file_path:
+        download_file(download_url, file_path)
+    else:
+        print("Download canceled")
